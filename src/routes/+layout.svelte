ca33230e8f75fb139f967c06c24c8c549e659570
mehedi-official <mehedi.tk.2020@gmail.com>
2025-01-27 22:58:26 +0600

    Initial Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import type { Snippet } from 'svelte';
+    import type { LayoutData } from './$types';
+    import '@fontsource-variable/inter';
+    import "../app.css";
+
+    let { data, children }: { data: LayoutData, children: Snippet } = $props();
+</script>
+
+{@render children()}
+
+<style>
+    :global(body) {
+        font-family: 'Inter Variable', sans-serif;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import About from "@component/sections/about/About.svelte";
+    import Header from "@component/sections/header/Header.svelte";
+</script>
+
+<Header />
+<main>
+    <About />
+</main>
+
+<svelte:head>
+    <title>Oram | Furniture For Your Life</title>
+</svelte:head>
